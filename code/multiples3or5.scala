252555b8ef48fef8bbeb8aaa97c0a144cf60aac2
entering <entering@gmail.com>
2013-10-22 01:23:28 +0100

    [ProjectEuler.net] Problem 1 beautify

--- code/multiples3or5.scala
+++ code/multiples3or5.scala
@@ -23,7 +23,7 @@ object Mult3or5 {
     if (num == 0)
       return 0
 
-    return ((num % 5 == 0 || num % 3 == 0) toInt) * num + mult3or5(num -1)
+    return ((num % 5 == 0 || num % 3 == 0) toInt) * num + mult3or5(num - 1)
   }
 }
 
