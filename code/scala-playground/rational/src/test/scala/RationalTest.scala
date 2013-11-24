f8f9e2c55ff4eaf84c9b6b46e8325e9b3d642391
entering <entering@gmail.com>
2013-11-24 12:12:38 +0000

    Change tests to funsuite

--- code/scala-playground/rational/src/test/scala/RationalTest.scala
+++ code/scala-playground/rational/src/test/scala/RationalTest.scala
@@ -1,31 +1,31 @@
-import org.scalatest.Suite
+import org.scalatest.FunSuite
 
-class RationalTest extends Suite {
-  def testAddRationals() {
+class RationalTest extends FunSuite {
+  test("Add rationals") {
     val r = new Rational(6, 12) + new Rational(24, 12)
     assert(r.numer == 5)
     assert(r.denom == 2)
   }
 
-  def testAddInt() {
+  test("Add integer to rational") {
     val r = new Rational(6, 12) + 2
     assert(r.numer == 5)
     assert(r.denom == 2)
   }
 
-  def testSubRationals() {
+  test("Subtract rationals") {
     val r = new Rational(6, 12) - new Rational(24, 12)
     assert(r.numer == -3)
     assert(r.denom == 2)
   }
 
-  def testSubInt() {
+  test("Subtract integer from rational") {
     val r = new Rational(18, 3) - 2
     assert(r.numer == 4)
     assert(r.denom == 1)
   }
 
-  def testToString() {
+  test("ToString of ration") {
     val r = new Rational(18, 3)
     assert("6/1" == r.toString)
   }
