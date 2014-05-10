3c8e0f6e2a8430794b3884b59cf2f292e059ddb2
entering <entering@gmail.com>
2014-05-10 15:54:45 +0100

    Improve code making use of lexical scope

--- code/scala-playground/squareroot/src/main/scala/squareroot/SquareRoot.scala
+++ code/scala-playground/squareroot/src/main/scala/squareroot/SquareRoot.scala
@@ -4,11 +4,11 @@ object SquareRoot {
   def sqrt(value: Double): Double = {
     def sqrtIt(guess: Double, origin: Double): Double = {
       def abs(x: Double) = if (x < 0) -x else x
-      def improveGuess(guess: Double, origin: Double) = (guess + origin / guess) / 2
-      def isGoodEnough(guess: Double, origin: Double) = abs(guess * guess - origin) / origin < 0.001
+      def improveGuess = (guess + origin / guess) / 2
+      def isGoodEnough = abs(guess * guess - origin) / origin < 0.001
 
-      if (isGoodEnough(guess, origin)) guess
-      else sqrtIt(improveGuess(guess, origin), origin)
+      if (isGoodEnough) guess
+      else sqrtIt(improveGuess, origin)
     }
 
     sqrtIt(1.0, value)
