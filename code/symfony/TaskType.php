bb4932dc7e512fbbcd8d952b8f758bca1c36847b
entering <eduardo@uniplaces.com>
2014-05-14 22:19:52 +0100

    Create TaskType.php

---
+++ code/symfony/TaskType.php
@@ -0,0 +1,19 @@
+<?php
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class TaskType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('task')
+            ->add('dueDate', null, array('widget' => 'single_text'))
+            ->add('save', 'submit');
+    }
+    
+    public function getName()
+    {
+        return 'task';
+    }
+}
