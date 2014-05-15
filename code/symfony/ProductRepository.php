bed06e564fb0a71b7949b0b3fcbdf2a652ca60cd
entering <eduardo@uniplaces.com>
2014-05-15 11:53:24 +0100

    Create ProductRepository.php

---
+++ code/symfony/ProductRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+use Doctrine\ORM\EntityRepository;
+
+class ProductRepository extends EntityRepository
+{
+    public function findAllOrderedByName()
+    {
+        return $this->getEntityManager()
+            ->createQuery(
+                'SELECT p FROM AcmeStoreBundle:Product p ORDER BY p.name ASC'
+            )
+            ->getResult();
+    }
+}
